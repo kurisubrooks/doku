dd6056dd6452f64c8671b3f932e4281eaeda6f6e
Kurisu Brooks <kurisubrooks@gmail.com>
2018-12-04 11:16:01 +1100

    Server Startup run async

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const Database = require("./core/Store");
 const Logger = require("./core/Util/Logger");
 const RequestHandler = require("./core/RequestHandler");
 
-// Initialise Database
-Database.load("database.json");
+(async() => {
+    // Initialise Database
+    await Database.load("database.json");
 
-// Start HTTP Server
-const http = require("http").createServer(server);
-const request = new RequestHandler(server); // eslint-disable-line no-unused-vars
+    // Start HTTP Server
+    const http = require("http").createServer(server);
+    const request = new RequestHandler(server); // eslint-disable-line no-unused-vars
 
-http.listen(9000, Logger.info("Core", "Listening on Port 9000"));
+    http.listen(9000, Logger.info("Core", "Listening on Port 9000"));
+})();
